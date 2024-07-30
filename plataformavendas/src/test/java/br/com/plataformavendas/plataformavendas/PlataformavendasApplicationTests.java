e7fa53647776a3744a7c2799dfbba21a65eb4de7
Everton Dantas <evertonbonfim@yahoo.com.br>
2024-07-30 18:15:05 -0300

    Iniciando

---
+++ plataformavendas/src/test/java/br/com/plataformavendas/plataformavendas/PlataformavendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.plataformavendas.plataformavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformavendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
