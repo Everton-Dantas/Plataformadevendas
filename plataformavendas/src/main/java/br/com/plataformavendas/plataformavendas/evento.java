f89b363dcf99bfae77bd27aec302846a2258a14a
Everton Dantas <evertonbonfim@yahoo.com.br>
2024-07-30 18:13:59 -0300

    Iniciando

---
+++ plataformavendas/src/main/java/br/com/plataformavendas/plataformavendas/evento.java
@@ -0,0 +1,15 @@
+package br.com.plataformavendas.plataformavendas;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class evento {
+    private Integer id;
+    private String nome;
+    private String descricao;
+}
---
+++ plataformavendas/src/main/java/br/com/plataformavendas/plataformavendas/inscricao.java
@@ -0,0 +1,15 @@
+package br.com.plataformavendas.plataformavendas;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class inscricao {
+    private Integer id;
+    private String nome;
+    private String email;
+}
