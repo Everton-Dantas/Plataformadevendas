83396be1225fd225a38c416952f05cce9be6117a
Isabelle Marcondes <isabelle.marcondes@hotmail.com>
2024-08-03 18:03:22 -0300

    Implementando tratamento de erro para body request inválido na funcionalidade Eventos

--- plataformavendas/src/main/java/br/com/plataformavendas/plataformavendas/Evento.java
+++ plataformavendas/src/main/java/br/com/plataformavendas/plataformavendas/Evento.java
@@ -1,7 +1,10 @@
 package br.com.plataformavendas.plataformavendas;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -18,6 +21,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Evento {
 
     @Id
@@ -31,10 +35,12 @@ public class Evento {
 
     @NotNull(message = "Data de início do evento é obrigatória")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @FutureOrPresent(message = "A data de início deve ser hoje ou no futuro")
     private LocalDate dataInicio;
 
     @NotNull(message = "Data de término do evento é obrigatória")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @FutureOrPresent(message = "A data de término deve ser hoje ou no futuro")
     private LocalDate dataTermino;
 
     @NotNull(message = "Preço do evento é obrigatório")
